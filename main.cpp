94d0b49988e95523cafe1a94da114c835b822574
AndoideXDD <andreupugibert7@gmail.com>
2024-01-24 23:45:08 +0100

    Anidating layers

--- main.cpp
+++ main.cpp
@@ -8,15 +8,22 @@ int main() {
     /*neuron firstNeuron;
     firstNeuron.SetupRandomNeuron();
     firstNeuron.printWeights(2);*/
-    layer firstlayer;
-    firstlayer.setupLayer(2);
-    firstlayer.showWeights(2);
+    layer firstlayer[2];
+    firstlayer[0].setupLayer(2);
+    firstlayer[0].showWeights(2);
     float input[] = {1,2};
     float out[2];
-    firstlayer.out(input,out,2);
+    firstlayer[0].out(input,out,2);
     for (int i = 0; i < 2; i++)
     {
         cout << "\n" <<out[i];
+    }
+    firstlayer[1].setupLayer(2);
+    firstlayer[1].showWeights(2);
+    firstlayer[1].out(out,input,2);
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "\n" <<input[i];
     }
     
 }
